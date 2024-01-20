e0dc7c3f0e74f49d70aba9d7f86b6f58362e4675
WrathOfZombies <bhargav.krishna@outlook.com>
2024-01-20 13:52:48 -0800

    chore: minor compilation thing

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,6 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
   extends: ["@scribe/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
